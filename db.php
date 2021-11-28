6d0d24eab38f516acf5fec8ffcef7fbd266f8792
perrot <linca30mus@gmail.com>
2021-11-28 19:58:00 +0800

    新增220資料庫連結

--- db.php
+++ db.php
@@ -1,7 +1,10 @@
 <?php
 function pdo($db){
-    $dsn="mysql:host=localhost;charset=utf8;dbname=$db";
-    return new PDO($dsn,'root','');
+    $dsn="mysql:host=localhost;charset=utf8;dbname=s1100423";
+    $pdo=new PDO($dsn,'s1100423','s1100423');
+    // $dsn="mysql:host=localhost;charset=utf8;dbname=$db";
+    // $pdo=new PDO($dsn,'root','')
+    return $pdo;
 }
 function findAll(){
     $pdo=pdo('db_story');
